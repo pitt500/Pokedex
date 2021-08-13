8803d59854e256aa6a82a915f7fdce43bd5bcc3a
pitt500 <>
2021-08-12 20:32:07 -0700

    Add Pokedex project

---
+++ Pokedex/Network/PokemonLoader.swift
@@ -0,0 +1,147 @@
+import Combine
+import Foundation
+
+enum PokemonError: Error, Identifiable {
+    case serverError
+    case noData
+
+    var id: Self {
+        self
+    }
+}
+
+class PokemonLoader: ObservableObject {
+    @Published var pokemonData: [Pokemon] = []
+    @Published var error = false
+
+    private var cancellable: AnyCancellable?
+    private let urlSession = URLSession(configuration: .default)
+    private let limit = 5
+    private var offset = 0
+
+    func combineLoad() {
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon/?limit=\(limit)&offset=\(offset)")!
+
+        cancellable = urlSession
+            .dataTaskPublisher(for: url)
+            .tryMap { output in
+                guard (output.response as? HTTPURLResponse)?.statusCode == 200
+                else { throw PokemonError.serverError }
+
+                return output.data
+            }
+            .decode(type: PokemonResponse.self, decoder: JSONDecoder())
+            .map(\.results)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else { return }
+                switch completion {
+                case .finished:
+                    self.error = false
+                    break
+                case .failure(let error):
+                    print(error)
+                    self.error = true
+                }
+            },receiveValue: { [weak self] response in
+                guard let self = self else { return }
+                print(response.map(\.name))
+                self.pokemonData = response.reversed() + self.pokemonData
+                self.offset += self.limit
+            })
+    }
+
+    func asyncLoad() async throws {
+        offset = 0
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon/?limit=\(limit)&offset=\(offset)")!
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard (response as? HTTPURLResponse)?.statusCode == 200
+        else { throw PokemonError.serverError }
+
+        guard let decoded = try? JSONDecoder().decode(PokemonResponse.self, from: data)
+        else { throw PokemonError.noData }
+
+        await updateList(newData: decoded.results)
+    }
+
+    @MainActor func updateList(newData: [Pokemon]) {
+        print(newData.map(\.name))
+        self.pokemonData = newData
+        self.offset += self.limit
+    }
+
+    func closureLoad(completion: @escaping (Result<[Pokemon], Error>) -> Void) {
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon/?limit=\(limit)&offset=\(offset)")!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+
+            if let error = error {
+                //We cannot throw an error in this completion handler!
+                completion(.failure(error))
+            } else if (response as? HTTPURLResponse)?.statusCode != 200 {
+                //We cannot throw an error in this completion handler!
+                completion(.failure(PokemonError.serverError))
+            } else {
+                guard let data = data
+                else {
+                    completion(.failure(PokemonError.noData))
+                    return
+                }
+
+                guard let pokemonResponse = try? JSONDecoder().decode(PokemonResponse.self, from: data)
+                else {
+                    completion(.failure(PokemonError.noData))
+                    return
+                }
+
+                completion(.success(pokemonResponse.results))
+            }
+        }
+
+        task.resume()
+    }
+
+    func getMorePokemons() async -> [Pokemon] {
+        self.offset += self.limit
+        
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon/?limit=\(limit)&offset=\(offset)")!
+        guard let (data, response) = try? await URLSession.shared.data(from: url)
+        else { return [] }
+
+        guard (response as? HTTPURLResponse)?.statusCode == 200
+        else { return [] }
+
+        guard let decoded = try? JSONDecoder().decode(PokemonResponse.self, from: data)
+        else { return [] }
+
+        return decoded.results
+    }
+
+    func load() async {
+        // Look how much you need to do using closures:
+//        closureLoad { [weak self] result in
+//            guard let self = self else { return }
+//
+//            DispatchQueue.main.async {
+//                switch result {
+//                case .success(let list):
+//                    self.pokemonData = list.reversed() + self.pokemonData
+//                    self.offset += self.limit
+//                case .failure(let error):
+//                    print(error)
+//                    self.error = true
+//                }
+//            }
+//        }
+
+        do {
+            try await asyncLoad()
+        } catch {
+            print(error)
+            self.error = true
+        }
+    }
+}
+
+
+
---
+++ Pokedex/Views/PokemonList.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+struct PokemonList: View {
+    @ObservedObject var loader: PokemonLoader
+    private let imageWidth = 110.0
+    private let cellHeight = 130.0
+
+    var body: some View {
+
+        List {
+            ForEach(loader.pokemonData) { pokemon in
+                AsyncImage(
+                    url: pokemon.url,
+                    scale: 1.0,
+                    transaction: Transaction(animation: .easeIn(duration: 0.5))
+                ) { phase in
+                    switch phase {
+                    case .success(let image):
+                        HStack {
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: imageWidth)
+                                .padding(.trailing, 10)
+                            PokemonDescriptionView(pokemon: pokemon)
+                            Spacer()
+                        }
+                    case .failure(let error):
+                        ErrorView(error: error)
+                    case .empty:
+                        HStack {
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle(tint: .red))
+                            Spacer()
+                        }
+                    @unknown default:
+                        // AsyncImagePhase is not marked as @frozen.
+                        // We need to support new cases in the future.
+                        Image(systemName: "questionmark")
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: cellHeight)
+                .padding()
+                .listRowSeparator(.hidden)
+                .task {
+                    if pokemon == loader.pokemonData.last {
+                        loader.pokemonData += await loader.getMorePokemons()
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonList(loader: PokemonLoader())
+    }
+}
